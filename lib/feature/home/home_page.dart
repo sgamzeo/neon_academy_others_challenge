21bcd2e8465d7b7b22d45fc9aae873234bc03d09
Gamze <89346098+sgamzeo@users.noreply.github.com>
2025-09-07 14:58:39 +0300

    feat(home): add homepage with navigation buttons to all feature pages

---
+++ lib/feature/home/home_page.dart
@@ -0,0 +1,43 @@
+import 'package:custom_components/components/buttons/custom_button.dart';
+import 'package:custom_components/constants/dimens.dart';
+import 'package:flutter/material.dart';
+import 'package:neon_others_challenge/feature/coral/coral_home_page.dart';
+import 'package:neon_others_challenge/feature/event_app/events.dart';
+import 'package:neon_others_challenge/feature/form_db/form_page.dart';
+import 'package:neon_others_challenge/feature/magic/ulfr_magic_home.dart';
+import 'package:neon_others_challenge/feature/music/music_page.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  void _navigate(BuildContext context, Widget page) {
+    Navigator.push(context, MaterialPageRoute(builder: (_) => page));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final pages = [
+      {'title': 'Coral Memory Fragments', 'page': CoralHomePage()},
+      {'title': 'Ticketmaster Events', 'page': const EventsPage()},
+      {'title': 'Heroes Form (SQLite)', 'page': HeroFormPage()},
+      {'title': "Ulfr's Magic (Animations)", 'page': const UlfrMagicHome()},
+      {'title': 'iTunes Song Search', 'page': const ITunesSearchPage()},
+    ];
+
+    return Scaffold(
+      appBar: AppBar(title: const Text('Neon Others Challenge')),
+      body: ListView.separated(
+        padding: Dimens.paddingLarge,
+        itemCount: pages.length,
+        separatorBuilder: (_, __) => SizedBox(height: Dimens.spaceXLarge),
+        itemBuilder: (context, index) {
+          final item = pages[index];
+          return CustomButton(
+            onPressed: () => _navigate(context, item['page'] as Widget),
+            text: item['title'] as String,
+          );
+        },
+      ),
+    );
+  }
+}
